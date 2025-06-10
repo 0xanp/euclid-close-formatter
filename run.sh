b4a915558dde193a513c9dfd8aaa14e8a2898cf8
McPepperoni <handsomepharaon@gmail.com>
2025-06-10 17:01:55 +0700

    added dcker scripts

---
+++ run.sh
@@ -0,0 +1,5 @@
+docker stop euclid-close-formatter || true
+docker rm euclid-close-formatter || true
+docker rmi euclid-close-formatter || true
+docker build -t euclid-close-formatter .
+docker run -d -p 3000:8501 --name euclid-close-formatter euclid-close-formatter
